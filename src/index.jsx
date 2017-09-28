9f67dec8ff077592eed1b0a7fa9251035ef9a072
Mateja Trifunovski <mtrifunovski@gmail.com>
2017-09-28 11:28:18 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -5,5 +5,3 @@ import { AppContainer } from 'react-hot-loader';
 import PkgTest from './pkg-test';
 
 render((<AppContainer><PkgTest /></AppContainer>), document.getElementById('app'));
-
-module.hot.accept();
